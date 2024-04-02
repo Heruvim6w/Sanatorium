532ca60a5bfb72f829211771d1478ba1e046cc5f
SalkovAndrei <andrei.salkov@searchanise.io>
2024-04-03 01:03:40 +0300

    Уведомление о регистрации

---
+++ resources/views/emails/admin_inbox.blade.php
@@ -0,0 +1,50 @@
+<table class="wrapper_mr_css_attr" role="presentation"
+       style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';-premailer-cellpadding: 0;-premailer-cellspacing: 0;-premailer-width: 100%;background-color: #edf2f7;margin: 0;padding: 0;width: 100%;"
+       width="100%" cellspacing="0" cellpadding="0">
+    <tbody>
+    <tr>
+        <td style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';"
+            align="center">
+            <table class="content_mr_css_attr" role="presentation"
+                   style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';-premailer-cellpadding: 0;-premailer-cellspacing: 0;-premailer-width: 100%;margin: 0;padding: 0;width: 100%;"
+                   width="100%" cellspacing="0" cellpadding="0">
+                <tbody>
+                    <tr>
+                        <td class="body_mr_css_attr" cellpadding="0" cellspacing="0"
+                            style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';-premailer-cellpadding: 0;-premailer-cellspacing: 0;-premailer-width: 100%;background-color: #edf2f7;border-bottom: 1px solid #edf2f7;border-top: 1px solid #edf2f7;margin: 0;padding: 0;width: 100%;"
+                            width="100%">
+                            <table class="inner-body_mr_css_attr" role="presentation"
+                                   style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';-premailer-cellpadding: 0;-premailer-cellspacing: 0;-premailer-width: 570px;background-color: #ffffff;border-color: #e8e5ef;border-radius: 2px;border-width: 1px;box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015);margin: 0 auto;padding: 0;width: 570px;"
+                                   width="570" cellspacing="0" cellpadding="0" align="center">
+
+                                <tbody>
+                                <tr>
+                                    <td class="content-cell_mr_css_attr"
+                                        style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';max-width: 100vw;padding: 32px;">
+                                        <h1 style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';color: #3d4852;font-size: 18px;font-weight: bold;margin-top: 0;text-align: left;">
+                                            Привет!</h1>
+                                        <p style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';font-size: 16px;line-height: 1.5em;margin-top: 0;text-align: left;">
+                                            На сайте Sanatorium новый пользователь
+                                        </p>
+                                        <p>Его имя {$user->name}</p>
+                                        <p>Его почта {$user->email}</p>
+                                        <p>Его id {$user->id}</p>
+                                        <p style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';font-size: 16px;line-height: 1.5em;margin-top: 0;text-align: left;">
+                                            Вам нужно одобрить регистрацию, чтобы он мог пользоваться всеми возможностями сайта</p>
+                                        <p style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';font-size: 16px;line-height: 1.5em;margin-top: 0;text-align: left;">
+                                            Для этого войдите в админ панель сайта и в разделе "Пользователи" найдите этого пользователя</p>
+                                        <p style="box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';font-size: 16px;line-height: 1.5em;margin-top: 0;text-align: left;">
+                                            С уважением,<br>
+                                            Sanatorium</p>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
